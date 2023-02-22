8aed06824e183b1bb9037e3e6511ffa59f4d73d2
Paukku <paula.hayrynen@hotmail.fi>
2023-02-22 21:58:09 +0200

    Little bit forward

--- ui/src/App.js
+++ ui/src/App.js
@@ -1,19 +1,31 @@
-import { useState, useEffect } from "react";
-import BlogForm from "./BlogForm";
-import Blogs from "./Blogs";
+import { useState, useEffect } from "react"
+import axios from 'axios'
+import BlogForm from "./components/BlogForm"
+import Blogs from "./components/Blogs"
 
 function App() {
   const [blogs, setBlogs] = useState([])
   const [newTitle, setNewTitle] = useState('')
   const [newAuthor, setNewAuthor] = useState('')
   const [newUrl, setNewUrl] = useState('')
-  const [newLike, setNewLike] = useState()
 
+  const hook = () => {
+    axios
+    .get('http://localhost:3001/blogs')
+    .then(response => {
+      setBlogs(response.data)
+    })
+  }
+  
+  useEffect(hook, [])
 
   const addBlog = (event) => {
     event.preventDefault()
     const blogObject = {title: newTitle, author: newAuthor, url:newUrl, like: 0}
     setBlogs(blogs.concat(blogObject))
+    setNewTitle('')
+    setNewAuthor('')
+    setNewUrl('')
   }
 
   const handleTitle = (event) => {
@@ -29,16 +41,13 @@ function App() {
   }
 
   const handleLikes = (event) => {
-    setNewLike(1)
-    blogs.filter(blog => blog.like = blog.like + 1)
-    
+    console.log(event.target.value) 
   }
 
   
   return (
     <div className="App">
       <BlogForm newTitle = {newTitle} newAuthor = {newAuthor} newUrl = {newUrl} addBlog = {addBlog} handleTitle = {handleTitle} handleAuthor = {handleAuthor} handleUrl = {handleUrl} />
-
       <Blogs blogs = {blogs} handleLikes={handleLikes} />
     </div>
   );
